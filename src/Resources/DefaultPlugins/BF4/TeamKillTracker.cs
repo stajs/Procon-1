02f5a8c29dafbad59908beece23921a1ee6ca8b8
stajs <shannonsnz@gmail.com>
2014-09-05 10:16:19 +1200

    Testing kick.

--- src/Resources/DefaultPlugins/BF4/TeamKillTracker.cs
+++ src/Resources/DefaultPlugins/BF4/TeamKillTracker.cs
@@ -5,82 +5,86 @@ using PRoCon.Core.Plugin;
 
 namespace PRoConEvents
 {
-    public class TeamKillTracker : PRoConPluginAPI, IPRoConPluginInterface
-    {
-        private string ReplaceStaches(string s)
-        {
-            return s.Replace("{", "~(").Replace("}", ")~");
-        }
+	public class TeamKillTracker : PRoConPluginAPI, IPRoConPluginInterface
+	{
+		private string ReplaceStaches(string s)
+		{
+			return s.Replace("{", "~(").Replace("}", ")~");
+		}
 
-        public void WriteConsole(string message)
-        {
-            ExecuteCommand("procon.protected.pluginconsole.write", ReplaceStaches(message));
-        }
+		private void WriteConsole(string message)
+		{
+			ExecuteCommand("procon.protected.pluginconsole.write", ReplaceStaches(message));
+		}
 
-        public override void OnPlayerTeamChange(string strSoldierName, int iTeamID, int iSquadID)
-        {
-            if (strSoldierName != "stajs")
-                return;
+		public override void OnPlayerTeamChange(string strSoldierName, int iTeamID, int iSquadID)
+		{
+			if (strSoldierName != "stajs")
+				return;
 
-            var message = string.Format("{0} joined team {1}.", strSoldierName, iTeamID);
+			var message = string.Format("{0} joined team {1}.", strSoldierName, iTeamID);
 
-            ExecuteCommand("procon.protected.send", "admin.say", message, "all");
-            ExecuteCommand("procon.protected.chat.write", "(AdminSay) " + message);
-        }
+			ExecuteCommand("procon.protected.send", "admin.say", message, "all");
+			ExecuteCommand("procon.protected.chat.write", "(AdminSay) " + message);
 
-        public void OnPluginLoaded(string strHostName, string strPort, string strPRoConVersion)
-        {
-            this.RegisterEvents(this.GetType().Name, "OnPlayerTeamChange");
-        }
+			ExecuteCommand("procon.protected.send", "admin.say", "Boot!", "all");
+			ExecuteCommand("procon.protected.chat.write", "(AdminSay) Boot!");
+			ExecuteCommand("procon.protected.send", "admin.kickPlayer", strSoldierName, "No more swapsies for you bro.");
+		}
 
-        public void OnPluginEnable()
-        {
-            WriteConsole("^bTeam Kill Tracker:^n ^2Enabled WTF.^0");
-        }
+		public void OnPluginLoaded(string strHostName, string strPort, string strPRoConVersion)
+		{
+			this.RegisterEvents(this.GetType().Name, "OnPlayerTeamChange");
+		}
 
-        public void OnPluginDisable()
-        {
-            WriteConsole("^bTeam Kill Tracker:^n ^8Disabled Cunt.^0");
-        }
+		public void OnPluginEnable()
+		{
+			WriteConsole("^bTeam Kill Tracker:^n ^2Enabled.^0");
+		}
 
-        public string GetPluginName()
-        {
-            return "Team Kill Tracker";
-        }
+		public void OnPluginDisable()
+		{
+			WriteConsole("^bTeam Kill Tracker:^n ^8Disabled.^0");
+		}
 
-        public string GetPluginVersion()
-        {
-            return "0.1.0";
-        }
+		public string GetPluginName()
+		{
+			return "Team Kill Tracker";
+		}
 
-        public string GetPluginAuthor()
-        {
-            return "stajs";
-        }
+		public string GetPluginVersion()
+		{
+			return "0.1.0";
+		}
 
-        public string GetPluginWebsite()
-        {
-            return "http://battlelog.battlefield.com/bf4/soldier/stajs/stats/904562646/pc/";
-        }
+		public string GetPluginAuthor()
+		{
+			return "stajs";
+		}
 
-        public string GetPluginDescription()
-        {
-            return "Pure awesomeness.";
-        }
+		public string GetPluginWebsite()
+		{
+			return "http://battlelog.battlefield.com/bf4/soldier/stajs/stats/904562646/pc/";
+		}
 
-        public List<CPluginVariable> GetDisplayPluginVariables()
-        {
-            return GetPluginVariables();
-        }
+		public string GetPluginDescription()
+		{
+			return "Pure awesomeness.";
+		}
 
-        public List<CPluginVariable> GetPluginVariables()
-        {
-            return new List<CPluginVariable>();
-        }
+		public List<CPluginVariable> GetDisplayPluginVariables()
+		{
+			return GetPluginVariables();
+		}
 
-        public void SetPluginVariable(string strVariable, string strValue)
-        {
+		public List<CPluginVariable> GetPluginVariables()
+		{
+			return new List<CPluginVariable>();
+		}
 
-        }
-    }
+		public void SetPluginVariable(string strVariable, string strValue)
+		{
+
+		}
+	}
 }
