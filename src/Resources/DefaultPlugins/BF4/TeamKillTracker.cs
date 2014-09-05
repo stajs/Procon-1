f6d00760bc78767babdf99f2b7b6c6a988d95081
stajs <shannonsnz@gmail.com>
2014-09-05 21:47:43 +1200

    Track killers and victims.

--- src/Resources/DefaultPlugins/BF4/TeamKillTracker.cs
+++ src/Resources/DefaultPlugins/BF4/TeamKillTracker.cs
@@ -1,144 +1,211 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using PRoCon.Core;
 using PRoCon.Core.Plugin;
 
 namespace PRoConEvents
 {
-	public class TeamKillTracker : PRoConPluginAPI, IPRoConPluginInterface
-	{
-		private static object _lock = new object();
-		private Dictionary<string, int> _teamSwaps = new Dictionary<string, int>();
+    public class TeamKillTracker : PRoConPluginAPI, IPRoConPluginInterface
+    {
+        private static object _lock = new object();
+        private List<Killer> _killers = new List<Killer>();
 
-		#region IPRoConPluginInterface
+        public class Killer
+        {
+            public string Name { get; set; }
+            public List<Victim> Victims { get; set; }
+        }
 
-		public void OnPluginLoaded(string strHostName, string strPort, string strPRoConVersion)
-		{
-			this.RegisterEvents(this.GetType().Name, "OnPlayerJoin", "OnPlayerKilled");
-		}
+        public class Victim
+        {
+            public string Name { get; set; }
+            public int Count { get; set; }
+        }
 
-		public void OnPluginEnable()
-		{
-			WriteConsole("^bTeam Kill Tracker:^n ^2Enabled.^0");
-		}
+        #region IPRoConPluginInterface
 
-		public void OnPluginDisable()
-		{
-			WriteConsole("^bTeam Kill Tracker:^n ^8Disabled.^0");
-		}
+        public void OnPluginLoaded(string strHostName, string strPort, string strPRoConVersion)
+        {
+            this.RegisterEvents(this.GetType().Name, "OnPlayerJoin", "OnPlayerKilled");
+        }
 
-		public string GetPluginName()
-		{
-			return "Team Kill Tracker";
-		}
+        public void OnPluginEnable()
+        {
+            WriteConsole("^bTeam Kill Tracker:^n ^2Enabled.^0");
 
-		public string GetPluginVersion()
-		{
-			return "0.1.0";
-		}
+            _killers = new List<Killer>();
+        }
 
-		public string GetPluginAuthor()
-		{
-			return "stajs";
-		}
+        public void OnPluginDisable()
+        {
+            WriteConsole("^bTeam Kill Tracker:^n ^8Disabled.^0");
+        }
 
-		public string GetPluginWebsite()
-		{
-			return "http://battlelog.battlefield.com/bf4/soldier/stajs/stats/904562646/pc/";
-		}
+        public string GetPluginName()
+        {
+            return "Team Kill Tracker";
+        }
 
-		public string GetPluginDescription()
-		{
-			return "Pure awesomeness.";
-		}
+        public string GetPluginVersion()
+        {
+            return "0.1.0";
+        }
 
-		public List<CPluginVariable> GetDisplayPluginVariables()
-		{
-			return GetPluginVariables();
-		}
+        public string GetPluginAuthor()
+        {
+            return "stajs";
+        }
 
-		public List<CPluginVariable> GetPluginVariables()
-		{
-			return new List<CPluginVariable>();
-		}
+        public string GetPluginWebsite()
+        {
+            return "http://battlelog.battlefield.com/bf4/soldier/stajs/stats/904562646/pc/";
+        }
 
-		public void SetPluginVariable(string strVariable, string strValue)
-		{
+        public string GetPluginDescription()
+        {
+            return "Pure awesomeness.";
+        }
 
-		}
+        public List<CPluginVariable> GetDisplayPluginVariables()
+        {
+            return GetPluginVariables();
+        }
 
-		#endregion
+        public List<CPluginVariable> GetPluginVariables()
+        {
+            return new List<CPluginVariable>();
+        }
 
-		#region PRoConPluginAPI
+        public void SetPluginVariable(string strVariable, string strValue)
+        {
 
-		public override void OnPlayerJoin(string player)
-		{
-			lock (_lock)
-			{
-				_teamSwaps.Remove(player);
-			}
+        }
 
-			WriteConsole(player + " joined.");
-		}
+        #endregion
 
-		public override void OnPlayerKilled(Kill kill) {
-			if (kill == null)
-				return;
+        #region PRoConPluginAPI
 
-			if (kill.Victim == null)
-				return;
+        public override void OnPlayerJoin(string player)
+        {
+            // TODO: Remove. For testing purposes, kill counts are reset on joining. Should be on round start.
+            lock (_lock)
+            {
+                _killers.RemoveAll(k => k.Name == player);
+            }
 
-			var victimName = kill.Victim.SoldierName;
+            WriteConsole(player + " joined.");
+        }
 
-			if (string.IsNullOrEmpty(victimName) || victimName != "stajs")
-				return;
+        public override void OnPlayerKilled(Kill kill)
+        {
+            var message = "Team Kill Tracker: OnPlayerKilled | ";
 
-			WriteConsole("stajs got wasted.");
-		}
+            if (kill == null || kill.Victim == null || kill.Killer == null)
+            {
+                WriteConsole(message + "Can not determine kill.");
+                return;
+            }
 
-		public override void OnPlayerTeamChange(string strSoldierName, int iTeamID, int iSquadID)
-		{
-			if (strSoldierName != "stajs")
-				return;
+            var victimName = kill.Victim.SoldierName;
+            var killerName = kill.Killer.SoldierName;
 
-			const int maxSwaps = 1;
+            // TODO: Remove safety check.
+            if (string.IsNullOrEmpty(victimName) || string.IsNullOrEmpty(killerName) || victimName != "stajs")
+            {
+                WriteConsole(message + "Can not determine name.");
+                return;
+            }
 
-			var count = 0;
+            // TODO: Uncomment. This is commented out to test fake TKs with suicides.
+            //if (kill.IsSuicide)
+            //{
+            //    WriteConsole(message + "Was suicide.");
+            //    return;
+            //}
 
-			lock (_lock)
-			{
-				if (_teamSwaps.TryGetValue(strSoldierName, out count))
-					count++;
+            var isTeamKill = kill.Killer.TeamID == kill.Victim.TeamID;
 
-				_teamSwaps[strSoldierName] = count;
-			}
+            if (!isTeamKill)
+            {
+                WriteConsole(message + "Not a TK.");
+                return;
+            }
 
-			var swapsLeft = maxSwaps - count;
-			var message = string.Format("{0} joined team {1}, swaps remaining before kick: {2}", strSoldierName, iTeamID, swapsLeft);
+            var killer = _killers.FirstOrDefault(k => k.Name == killerName);
 
-			ExecuteCommand("procon.protected.send", "admin.say", message, "all");
-			ExecuteCommand("procon.protected.chat.write", "(AdminSay) " + message);
+            if (killer == null)
+            {
+                killer = new Killer
+                {
+                    Name = killerName,
+                    Victims = new List<Victim>()
+                };
 
-			if (swapsLeft > 0)
-				return;
+                _killers.Add(killer);
+            }
 
-			message = "Say bye-bye to stajs. Kicked in 5 seconds.";
+            var victim = killer.Victims.FirstOrDefault(v => v.Name == victimName);
 
-			ExecuteCommand("procon.protected.send", "admin.say", message, "all");
-			ExecuteCommand("procon.protected.chat.write", "(AdminSay) " + message);
-			ExecuteCommand("procon.protected.tasks.add", "TeamKillTracker", "5", "1", "1", "procon.protected.send", "admin.kickPlayer", strSoldierName, "Boot!");
-		}
+            if (victim == null)
+            {
+                victim = new Victim
+                {
+                    Name = victimName,
+                    Count = 0
+                };
 
-		#endregion
+                killer.Victims.Add(victim);
+            }
 
-		private string ReplaceStaches(string s)
-		{
-			return s.Replace("{", "~(").Replace("}", ")~");
-		}
+            victim.Count++;
 
-		private void WriteConsole(string message)
-		{
-			ExecuteCommand("procon.protected.pluginconsole.write", ReplaceStaches(message));
-		}
-	}
+            WriteConsole(string.Format("{0}{1} has TK'd {2} {3} times.", message, killerName, victimName, victim.Count));
+        }
+
+        //public override void OnPlayerTeamChange(string strSoldierName, int iTeamID, int iSquadID)
+        //{
+        //    if (strSoldierName != "stajs")
+        //        return;
+
+        //    const int maxSwaps = 1;
+
+        //    var count = 0;
+
+        //    lock (_lock)
+        //    {
+        //        if (_teamSwaps.TryGetValue(strSoldierName, out count))
+        //            count++;
+
+        //        _teamSwaps[strSoldierName] = count;
+        //    }
+
+        //    var swapsLeft = maxSwaps - count;
+        //    var message = string.Format("{0} joined team {1}, swaps remaining before kick: {2}", strSoldierName, iTeamID, swapsLeft);
+
+        //    ExecuteCommand("procon.protected.send", "admin.say", message, "all");
+        //    ExecuteCommand("procon.protected.chat.write", "(AdminSay) " + message);
+
+        //    if (swapsLeft > 0)
+        //        return;
+
+        //    message = "Say bye-bye to stajs. Kicked in 5 seconds.";
+
+        //    ExecuteCommand("procon.protected.send", "admin.say", message, "all");
+        //    ExecuteCommand("procon.protected.chat.write", "(AdminSay) " + message);
+        //    ExecuteCommand("procon.protected.tasks.add", "TeamKillTracker", "5", "1", "1", "procon.protected.send", "admin.kickPlayer", strSoldierName, "Boot!");
+        //}
+
+        #endregion
+
+        private string ReplaceStaches(string s)
+        {
+            return s.Replace("{", "~(").Replace("}", ")~");
+        }
+
+        private void WriteConsole(string message)
+        {
+            ExecuteCommand("procon.protected.pluginconsole.write", ReplaceStaches(message));
+        }
+    }
 }
